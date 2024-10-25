abef1fd094ccb5c6e389b67b9ec890a2e97cb3c2
eduardotecnologo <eduardotecnologo@hotmail.com>
2024-10-24 21:21:22 -0300

    feat: create api

---
+++ api/src/repositories/repository.doctor.js
@@ -0,0 +1,9 @@
+async function Listar(){
+    
+    const doctors = [
+        {id: 1, name:"Eduardo", specialty: "Developer", icon:"M"},
+        {id: 2, name:"Alexandre", specialty: "Developer", icon:"M"},
+        {id: 3, name:"Lucio", specialty: "Developer", icon:"M"},]
+    return doctors;
+}
+export default {Listar};
